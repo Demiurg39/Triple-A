d398aec648b09fe940d64d52da7dce7036b636dd
Demiurg39 <chiryagov2014@gmail.com>
2023-11-20 00:19:34 +0600

    added urls to password reset/change

--- account/urls.py
+++ account/urls.py
@@ -1,15 +1,46 @@
 from django.contrib.auth import views as auth_views
-from django.urls import include, path
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
     # URL for login
-    path("login/", auth_views.LoginView.as_view, name="login"),
+    path("login/", auth_views.LoginView.as_view(), name="login"),
     # URL for logout
-    path("logout/", auth_views.LogoutView.as_view, name="logout"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     # URL for registration
     path("register/", views.register, name="register"),
     # URL to reset, change password
-    path("", include("django.contrib.auth.urls")),
+    # password change
+    path(
+        "password-change/",
+        auth_views.PasswordChangeView.as_view(),
+        name="password_change",
+    ),
+    path(
+        "password-change-done/",
+        auth_views.PasswordChangeDoneView.as_view(),
+        name="password_change_done",
+    ),
+    # password reset
+    path(
+        "password-reset/",
+        auth_views.PasswordResetView.as_view(),
+        name="password_reset",
+    ),
+    path(
+        "password-reset-done/",
+        auth_views.PasswordResetDoneView.as_view(),
+        name="password_reset_done",
+    ),
+    path(
+        "password-reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
+    path(
+        "password_reset/complete/",
+        auth_views.PasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
+    ),
 ]
