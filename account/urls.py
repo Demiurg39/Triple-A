b3df544cb537ee4160c01c6a8f500455baee9a42
Demiurg39 <chiryagov2014@gmail.com>
2023-11-19 23:33:55 +0600

    added login logout register urlpath

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,15 @@
 from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.urls import include, path
 
 from . import views
 
 urlpatterns = [
+    # URL for login
     path("login/", auth_views.LoginView.as_view, name="login"),
+    # URL for logout
     path("logout/", auth_views.LogoutView.as_view, name="logout"),
+    # URL for registration
+    path("register/", views.register, name="register"),
+    # URL to reset, change password
+    path("", include("django.contrib.auth.urls")),
 ]
