9b2447a80397a60fd7debc7ea4fea1599e8f4ed0
Demiurg39 <chiryagov2014@gmail.com>
2023-11-23 13:49:50 +0600

    added dublicate email check

--- account/forms.py
+++ account/forms.py
@@ -26,14 +26,26 @@ class UserRegistrationForm(forms.ModelForm):
     def clean_password2(self):
         cd = self.cleaned_data
         if cd["password"] != cd["password2"]:
-            raise forms.ValidatorError("Passwords doesn't match.")
+            raise forms.ValidationError("Passwords doesn't match.")
         return cd["password"]
+
+    def clean_email(self):
+        data = self.cleaned_data["email"]
+        if User.objects.filter(email=data).exists():
+            raise forms.ValidationError("Email already in use.")
 
 
 class UserEditForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ["first_name", "last_name", "email"]
+
+    def clean_email(self):
+        data = self.cleaned_data["email"]
+        qs = User.objects.exclude(id=self.instance.id).filter(email=data)
+        if qs.exists():
+            raise forms.ValidationError("Email already in use.")
+        return data
 
 
 class ProfileEditForm(forms.ModelForm):
