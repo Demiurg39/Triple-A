1012581c06c00c3ad4718b3589368f1aed445bc0
Demiurg39 <chiryagov2014@gmail.com>
2023-11-23 13:23:21 +0600

    create profile model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,18 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+def path_to_profile_photo(instance, filename):
+    return f"profiles/{instance.user.username}/{filename}"
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    date_of_birth = models.DateField(blank=True, null=True)
+    photo = models.ImageField(upload_to=path_to_profile_photo)
+
+    def __str__(self):
+        return f"Profile of {self.user.username}"
