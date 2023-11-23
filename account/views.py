ea65112bb147607ca39d08abcae908d468231c6b
Demiurg39 <chiryagov2014@gmail.com>
2023-11-23 13:31:07 +0600

    added edit view. Create edit view template

--- account/views.py
+++ account/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-from .forms import UserRegistrationForm
+from .forms import ProfileEditForm, UserEditForm, UserRegistrationForm
+from .models import Profile
 
 
 def register(request):
@@ -10,6 +12,7 @@ def register(request):
             new_user = user_form.save(commit=False)
             new_user.set_password(user_form.cleaned_data["password"])
             new_user.save()
+            Profile.objects.create(user=new_user)
             return render(
                 request,
                 "account/register_done.html",
@@ -22,3 +25,26 @@ def register(request):
             "account/register.html",
             {"user_form": user_form},
         )
+
+
+@login_required
+def edit(request):
+    if request.method == "POST":
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(
+            instance=request.user.profile,
+            data=request.POST,
+            files=request.FILES,
+        )
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+
+    return render(
+        request,
+        "account/edit.html",
+        {"user_form": user_form, "profile_form": profile_form},
+    )
