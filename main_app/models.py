b59b95a53c817fe33950021135e80766a2f7f978
Demiurg39 <chiryagov2014@gmail.com>
2023-12-05 13:25:48 +0600

    inital commit

--- main_app/models.py
+++ main_app/models.py
@@ -1,12 +1,32 @@
-# from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.urls import reverse
 from django.utils import timezone
-from django.contrib.auth.models import User
-import os
+
 
 def poster_upload_path(instance, filename):
-    return os.path.join("Games_images", instance.slug, filename)
+    return f"Games_images/{instance.slug}/{filename}"
+
+
+def validate_float_range(value):
+    if not 0 < value < 100:
+        raise ValidationError("Value must be between 0 and 100.")
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150)
+    slug = models.SlugField(max_length=150, unique=True)
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+        ordering = ["name"]
+        indexes = [models.Index(fields=["name"])]
+
+    def __str__(self):
+        return self.name
+
 
 class SystemRequirements(models.Model):
     OS_version = models.CharField("Версия ОП", max_length=100)
@@ -21,31 +41,51 @@ class SystemRequirements(models.Model):
         verbose_name = "SystemRequirement"
         verbose_name_plural = "SystemRequirements"
 
+
 class Games(models.Model):
-    title = models.CharField("Game title", max_length=50)
-    slug = models.SlugField("Slug field", max_length=50)
-    rating = models.FloatField("Game rating")
-    description = models.TextField("Game description")
-    poster = models.ImageField("Game poster", upload_to=poster_upload_path)
-    rent = models.FloatField("Rent cost per weak")
-    buy = models.FloatField("Cost")
+    name = models.CharField("Game title", max_length=250)
+    slug = models.SlugField("Slug field", max_length=250)
+    rating = models.FloatField("Game rating", validators=[validate_float_range])
+    description = models.TextField("Game description", blank=True)
+    image = models.ImageField(
+        "Game poster",
+        upload_to=poster_upload_path,
+        blank=True,
+    )
+    rent = models.FloatField("Rent price per weak")
+    price = models.DecimalField("Price", max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
     systemreq = models.ForeignKey(
         SystemRequirements,
         on_delete=models.CASCADE,
-        related_name="game_requirements",
+        related_name="game_req",
     )
-    added = models.DateTimeField(default=timezone.now)
+    categories = models.ManyToManyField(
+        Category,
+        related_name="categories",
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     class Meta:
         verbose_name = "Game"
         verbose_name_plural = "Games"
-        ordering = ["-added"]
+        ordering = ["name"]
+        indexes = [
+            models.Index(fields=["id", "slug"]),
+            models.Index(fields=["name"]),
+            models.Index(fields=["-created"]),
+        ]
 
     def __str__(self):
-        return self.title
+        return self.name
 
     def get_absolute_url(self):
-        return reverse('main_app:game_detail', args=[str(self.slug)])
+        return reverse(
+            "main_app:game_detail",
+            args=[self.id, self.slug],
+        )
+
 
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -55,9 +95,13 @@ class Comment(models.Model):
     active = models.BooleanField(default=True)
 
     class Meta:
-        ordering = ['created_at']
+        ordering = ["created_at"]
         indexes = [
-            models.Index(fields=['created_at']),
+            models.Index(fields=["created_at"]),
+        ]
+        ordering = ["created_at"]
+        indexes = [
+            models.Index(fields=["created_at"]),
         ]
 
     def __str__(self):
