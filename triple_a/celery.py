9fe54bc5a9c7f9483d2bfcd4f07eedeec83db786
Demiurg39 <chiryagov2014@gmail.com>
2023-12-10 14:28:13 +0600

    added celery worker

--- triple_a/celery.py
+++ triple_a/celery.py
@@ -4,5 +4,5 @@ from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "triple_a.settings")
 app = Celery("triple_a")
-app.config_from_objects("django.conf:settings", namespace="CELERY")
-app.autodiscover.tasks()
+app.config_from_object("django.conf:settings", namespace="CELERY")
+app.autodiscover_tasks()
